5a100f3a53437d28ca747e869f8006d90fb036b3
Amauri Carlos <amauricarlosdecarvalho@gmail.com>
2025-11-06 19:52:20 -0300

    add initial files

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Pagina de Login</title>
+</head>
+<body>
+       <h1>Realizar Login</h1>
+
+     <form method="POST" action="">
+        <label>Usuário: </label><input type="text" name="usuario" required><br><br>
+        <label>Senha: </label><input type="password" name="senha" required><br><br>
+
+        <input type="submit" value="Entrar"><br><br>
+
+        <a href="forgotPassword.php">Esqueci minha senha</a>
+
+     </form>
+        
+
+
+
+
+
+
+    
+</body>
+</html>
