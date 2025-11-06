75a23836131a6f97620696d9846cf68c269a810e
Amauri Carlos <amauricarlosdecarvalho@gmail.com>
2025-11-06 20:18:53 -0300

    add forms

---
+++ forgotPassword.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Esqueci a senha</title>
+</head>
+<body>
+    <a href="index.php" class="voltar">Voltar à pagina inicial</a>
+       <h1>Recuperação de senha</h1>
+
+     <form method="post" action="">
+        <label for="email"> E-mail de recuperação:</label><br>
+        <input type="text" name="email"><br><br>
+
+        <input type="submit" value="Resetar senha">
+
+
+     </form>
+        
+
+
+
+
+
+
+    
+</body>
+</html>
---
+++ users.php
@@ -0,0 +1,9 @@
+<?php
+
+$usuarios = [
+    'Amauri' => '$2y$10$6CFxd10/hkGLNZGEUoDi4eb2XUUaT1SC7Txb92aYx6XU9mvomWoB' //1234
+];
+
+
+
+?>
